199de1023dc723a6f8ef658b1329121afa49b5c3
asketonim <44112567+asketonim@users.noreply.github.com>
2018-12-31 21:26:10 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var s, m, h;
+function setup() {
+  createCanvas(1400, 750);
+}
+
+
+function draw() {
+  translate(300, 370);
+  background(0);
+  s = second();
+  m = minute();
+  h = hour();
+
+
+  angleMode(DEGREES);
+  stroke("#c8e4fe");
+  strokeWeight(8);
+  rotate(s*6);
+  line(0,0, 0, -130);
+  rotate(-s*6);
+  stroke("#94d2e6");
+  rotate(m*6);
+  line(0,0, 0, -120);
+  rotate(-m*6);
+  stroke("#a5aeff");
+  rotate(h * (360 / 24));
+  line(0,0, 0, -90);
+  rotate(-h * (360 / 24));
+  stroke(100, 0, 200);
+  point(0, 0);
+  stroke("#7dd8c7");
+  line(0, 140, 0, 150);
+  line(0, -140, 0, -150);
+  line(140, 0, 150, 0);
+  line(-140, 0, -150, 0);
+
+}
